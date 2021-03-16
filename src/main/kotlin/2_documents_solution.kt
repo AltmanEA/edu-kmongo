a55523bb332797077b2b7a269473628618b8c1c4
Евгений Альтман <altmanea@gmail.com>
2021-03-16 09:27:02 +0600

    document solution finish

--- src/main/kotlin/2_documents_solution.kt
+++ src/main/kotlin/2_documents_solution.kt
@@ -1,33 +1,41 @@
 import org.litote.kmongo.*
 
-fun incGrade(courseName: String, studentName: String) =
+fun incGrade(course: String, studentName: String) =
     mCourses.updateOne(
         and(
-            Course::name eq courseName,
+            Course::name eq course,
             Course::grades / Grade::studentName eq studentName
         ),
         inc(Course::grades.posOp / Grade::value, 1)
     )
 
-fun addStudent(name: String, group: String, subjs: List<String>){
+fun addStudent(name: String, group: String, courses: List<String>) {
     val student = Student(name, group)
     mStudents.insertOne(student)
     mCourses.updateMany(
-        Course::name `in` subjs,
+        Course::name `in` courses,
         push(Course::grades, Grade(student.id, student.name))
     )
 }
 
-fun main(){
+fun setAllValue(course: String, value: Int) =
+    mCourses.updateOne(
+        Course::name eq course,
+        setValue(Course::grades.allPosOp / Grade::value, value)
+    )
+
+fun main() {
     fillStudentsAndCourse()
     setGrade("Math", "Penny", 5)
     setGrade("Math", "Sheldon", 6)
     prettyPrintCursor(mCourses.find(Course::name eq "Math"))
-    incGrade("Math","Sheldon")
+    incGrade("Math", "Sheldon")
     prettyPrintCursor(mCourses.find(Course::name eq "Math"))
 //    incGrade("Math","Raj")
 //    prettyPrintCursor(mCourses.find(Course::name eq "Math"))
     addStudent("Stuart", "Boys", listOf("Math", "Phys"))
     addStudent("Emily", "Girls", listOf("Math", "Chem"))
     prettyPrintCursor(mCourses.find())
+    setAllValue("Math", 3)
+    prettyPrintCursor(mCourses.find(Course::name eq "Math"))
 }
